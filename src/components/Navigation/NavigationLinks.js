c09e691bbe3286e0d0539f4717d42a25a4fe1d68
EduardoMontanha <edumontanha14@gmail.com>
2020-05-13 23:39:15 -0300

    Create basis for new component

---
+++ src/components/Navigation/NavigationLinks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class NavigationLinks extends React.Component {
+
+    render () {
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default NavigationLinks;
