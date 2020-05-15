215d1f545433a290809b37ce47366f615df94820
EduardoMontanha <edumontanha14@gmail.com>
2020-05-15 01:33:26 -0300

    Create Product component

---
+++ src/components/Product/Product.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Product extends React.Component {
+
+    render () {
+        return(
+            <section id="product">
+                <div className="container"></div>
+            </section>
+        );
+    }
+}
+
+export default Product;
