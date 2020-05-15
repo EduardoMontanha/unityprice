e8cb39d5b13c55f7932765cda070024da5629e14
EduardoMontanha <edumontanha14@gmail.com>
2020-05-15 15:52:58 -0300

    Finish modal component

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -11,8 +11,10 @@ class Modal extends React.Component {
     render () {
         return(
             <div className="modal hide">
-                <div className="close-modal" onClick={(e) => closeModal(e)}>&#215;</div>
-                <div className="modal-content"></div>
+                <div className="close-modal" onClick={(e) => closeModal(e)}>&#10005;</div>
+                <div className="modal-content">
+                    {this.props.children}
+                </div>
             </div>
         );
     }
