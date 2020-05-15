38c8c4191288c28ee49d472ea43f9f7cafcfb8bc
EduardoMontanha <edumontanha14@gmail.com>
2020-05-15 17:44:29 -0300

    Create header structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import { LanguageContext } from '../../context/LanguageContext';
+import shareIcon from '../../assets/images/icons/share-icon.png';
 
 
 class Header extends React.Component {
@@ -13,7 +13,21 @@ class Header extends React.Component {
         return(
             <header id="home">
                 <div className="container">
+                    <h1>
+                        {headerData.welcome}
+                        <span>Unity Price</span>
+                    </h1>
 
+                    <p>{headerData.description}</p>
+
+                    <div>
+                        <button id="start-btn">{headerData.start}</button>
+
+                        <button id="share-btn">
+                            <span>{headerData.share}</span>
+                            <img src={shareIcon} alt="Share"/>
+                        </button>
+                    </div>
                 </div>
             </header>
         );
