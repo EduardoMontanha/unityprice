38e61dbb7cf6d4bc21009e90d9de1177a28316eb
EduardoMontanha <edumontanha14@gmail.com>
2020-05-18 19:23:51 -0300

    Start creating product section

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { LanguageContext } from '../../context/LanguageContext';
 
 
 class Product extends React.Component {
 
+    static contextType = LanguageContext;
+
     render () {
+        let productData = this.context.product;
+
         return(
             <section id="product">
-                <div className="container"></div>
+                <div className="container">
+                    <h2>{productData.title}</h2>
+
+                    <p className="description">{productData.description}</p>
+
+                    <label htmlFor="prod-name">{productData.nameLabel}</label>
+                    <input id="prod-name" type="text" placeholder={productData.namePlaceholder}/>
+
+                    <label htmlFor="prod-yield">{productData.yieldLabel}</label>
+                    <input id="prod-yield" type="number" placeholder="3"/>
+                    <span>{productData.yieldDescription}</span>
+                </div>
             </section>
         );
     }
