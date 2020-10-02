c4d1c1fb2cadbf98f034fdafeb6cde8ab669f6ee
EduardoMontanha <edumontanha14@gmail.com>
2020-10-01 22:29:21 -0300

    Add language provider

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { LanguageProvider } from './contexts/LanguageContext';
 import Footer from './routes/components/Footer';
 import Header from './routes/components/Header';
 import Home from './routes/Home/Home';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Header />
+        <LanguageProvider>
+            <BrowserRouter>
+                <Header />
 
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/sobre" exact component={Home} />
-            </Switch>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/sobre" exact component={Home} />
+                </Switch>
 
-            <Footer />
-        </BrowserRouter>
+                <Footer />
+            </BrowserRouter>
+        </LanguageProvider>
     );
 }
 
