cb397844d473dbb5b5e7ed7c0dd940238e0ea0ec
EduardoMontanha <edumontanha14@gmail.com>
2020-10-01 22:31:35 -0300

    Finish header

--- src/routes/components/Header.jsx
+++ src/routes/components/Header.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import useWindowSize from '../../hooks/useWindowSize';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.svg';
+import Text from './Text';
+import LanguageSelector from './LanguageSelector';
 
 function Header() {
     const size = useWindowSize();
     const [isMobile, setIsMobile] = useState(true);
     const [hideMenu, setHideMenu] = useState(true);
+    const pageId = "header";
 
     useEffect(() => {
         if (size.width >= 768) {
@@ -34,7 +37,7 @@ function Header() {
                     </Link>
                 </div>
 
-                <button className={`nav-bar-toggler ${isMobile ? '' : 'hide'}`} onClick={toggleMenu}>
+                <button className={`nav-bar-toggler ${isMobile ? '' : 'hide'}`} onClick={() => toggleMenu()}>
                     <div id="burger-icon">
                         <div className="bar bar1"></div>
                         <div className="bar bar2"></div>
@@ -45,10 +48,17 @@ function Header() {
                 <nav className={`nav-links ${hideMenu ? 'hide' : ''}`}>
                     <ul>
                         <li className="nav-item">
-                            <Link to="/">Home</Link>
+                            <Link to="/" onClick={() => toggleMenu()}>
+                                <Text pageId={pageId} pageType={"navigation"} tid="home" />
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/sobre">Sobre</Link>
+                            <Link to="/sobre" onClick={() => toggleMenu()}>
+                                <Text pageId={pageId} pageType={"navigation"} tid="about" />
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <LanguageSelector />
                         </li>
                     </ul>
                 </nav>
