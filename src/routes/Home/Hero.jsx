95ab4f0a268d5306a829098de876d1e19ef8ed77
EduardoMontanha <edumontanha14@gmail.com>
2020-10-02 22:47:30 -0300

    Create hero component

--- src/routes/Home/Hero.jsx
+++ src/routes/Home/Hero.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import Text from '../components/Text';
 
 function Hero() {
+    const pageId = "hero"
+
+    const scrollNext = () => {
+        document.querySelector('#product-name').scrollIntoView({ 
+            behavior: 'smooth' 
+        });
+    }
 
     return(
-        <section></section>
+        <section id="hero">
+            <div className="container">
+                <div className="title">
+                    <h2><Text pageId={pageId} tid="h2" /></h2>
+                    <h1>Unity Price</h1>
+                    
+                </div>
+
+                <p className="description">
+                    <Text pageId={pageId} tid="description" />
+                </p>
+
+                <button className="cta-start" onClick={() => scrollNext()}>
+                    <Text pageId={pageId} tid="cta" />
+                </button>
+            </div>
+        </section>
     );
 }
 
