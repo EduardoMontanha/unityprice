c0a672ad140c26b886bb1a3ed855a621f8c19152
EduardoMontanha <edumontanha14@gmail.com>
2020-10-02 23:37:47 -0300

    Create first section of product details
    Still need to add some style to it

--- src/routes/Home/ProductDetails.jsx
+++ src/routes/Home/ProductDetails.jsx
@@ -5,12 +5,35 @@ import { ProductContext } from '../../contexts/ProductContext';
 function ProductDetails() {
     const product = useContext(ProductContext);
 
-    console.log(product);
+    const scrollTo = element => {
+        document.querySelector(element).scrollIntoView({ 
+            behavior: 'smooth' 
+        });
+    }
 
     return(
         <>
             <section id="product-name">
-                <h2></h2>
+                <div className="container">
+                    <h2>O que é o seu produto?</h2>
+
+                    <p>
+                        Conte para nós o que é o seu produto.<br />
+                        Um exemplo: Bolo de Cenoura.
+                    </p>
+
+                    <input type="text" />
+
+                    <div className="scroll-ctas">
+                        <button onClick={() => scrollTo("#resources")}>Próximo</button>
+                    </div>
+                </div>
+            </section>
+
+            <section id="resources">
+                <div className="container">
+                    
+                </div>
             </section>
         </>
     );
