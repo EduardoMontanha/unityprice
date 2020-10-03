851caaecb8d4ac995fd9cc6fd0fbe285dd289a3e
EduardoMontanha <edumontanha14@gmail.com>
2020-10-02 22:48:48 -0300

    Create home component

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Hero from './Hero';
+import ProductDetails from './ProductDetails';
 
 function Home() {
 
     return(
         <>
             <Hero />
+
             <main>
-                
+                <ProductDetails />
             </main>
         </>
     );
